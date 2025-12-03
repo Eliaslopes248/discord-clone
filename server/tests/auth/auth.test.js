7454b8cfe04566f8797deec9549161772989d8ff
Elias Lopes <ealopes@aggies.ncat.edu>
2025-12-03 12:32:18 -0500

    shh

--- server/tests/auth/auth.test.js
+++ server/tests/auth/auth.test.js
@@ -1,2 +1,12 @@
 
-console.log("Running tests!!");
+function test(num){
+    return num ==0;
+}
+
+describe("Testing authentication system", ()=>{
+
+    it("testing example func", ()=>{
+        expect(test(2)).toBe(false);
+    });
+
+});
